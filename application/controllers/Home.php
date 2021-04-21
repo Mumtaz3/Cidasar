e5ecf56ca33a54143f3011ab823a974b13e42711
Mumtaz3 <abdullahmumtaz183@gmail.com>
2021-04-21 20:14:13 +0700

    belajar ci

---
+++ application/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+class Home extends CI_Controller {
+
+	public function home()
+	{
+		$this->load->view('template');
+	}
+	public function about(){
+		$this->load->view('about');
+	}
+    public function profile(){
+        $this->load->view('profile');
+    }
+}
